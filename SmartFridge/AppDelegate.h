bf0293835580909b9cdb159ae73a289fd46f53ee
yallenh <yanhsianghuang@gmail.com>
2019-10-11 16:37:20 -0700

    Initial Commit

---
+++ SmartFridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartFridge
+//
+//  Created by 黃彥翔 on 2019/10/11.
+//  Copyright © 2019 黃彥翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartFridge/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SmartFridge
+//
+//  Created by 黃彥翔 on 2019/10/11.
+//  Copyright © 2019 黃彥翔. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
