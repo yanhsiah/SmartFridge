a7b77184f33236645b2e11489068faefe02e0a7d
yallenh <yanhsianghuang@gmail.com>
2019-10-21 14:39:43 -0700

    init root vc

---
+++ SmartFridge/SFRootViewController.h
@@ -0,0 +1,17 @@
+//
+//  SFRootViewController.h
+//  SmartFridge
+//
+//  Created by 黃彥翔 on 2019/10/21.
+//  Copyright © 2019 黃彥翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SFRootViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
